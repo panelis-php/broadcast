5d19858f3e33822b930daf275f99e6a30a799740
Yugo <dedy.purwanto@levart.id>
2026-08-31 09:25:09 +0700

    Register permission

---
+++ lang/en/permission.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'name_browse_broadcast' => 'Browse broadcasts',
+    'name_create_broadcast' => 'Create broadcasts',
+    'name_edit_broadcast' => 'Edit broadcasts',
+    'name_delete_broadcast' => 'Delete broadcasts',
+];
---
+++ lang/id/permission.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'name_browse_broadcast' => 'Lihat siaran',
+    'name_create_broadcast' => 'Buat siaran',
+    'name_edit_broadcast' => 'Ubah siaran',
+    'name_delete_broadcast' => 'Hapus siaran',
+];
